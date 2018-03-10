6b644d15760ac6bdeb7f7a2536f25d96a25a3eee
shangjingbo1226 <shangjingbo@gmail.com>
2018-03-10 15:10:31 -0600

    add test cases

---
+++ problems/h-combinatorics/testcases/gen.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cassert>
+#include <string>
+using namespace std;
+
+int main()
+{
+    srand(19910724);
+
+    for (int testcase = 0; testcase < 10; ++ testcase) {
+        char filename[100];
+        sprintf(filename, "combinatorics.%d.in", testcase);
+        FILE* out = fopen(filename, "w");
+        int n = testcase < 5 ? 1000 : 100000;
+        fprintf(out, "%d\n", n);
+        for (int i = 0; i < n; ++ i) {
+            fprintf(out, "%d %d %d %d\n", rand() % 1000 + 1, rand() % 1000 + 1, rand() % 1000 + 1, rand() % 1000 + 1);
+        }
+        fclose(out);
+
+        char ansfilename[100];
+        sprintf(ansfilename, "combinatorics.%d.ans", testcase);
+        char command[200];
+        sprintf(command, "../a.out < %s > %s", filename, ansfilename);
+        system(command);
+    }
+    return 0;
+}
