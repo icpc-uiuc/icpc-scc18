b11190b2c73794eb1c930c4dc495fb8ab5838373
Liu Jiacheng <liujch1998@gmail.com>
2018-04-03 23:40:48 -0500

    F time limit change

--- problems/f-animal/solcmp.sh
+++ problems/f-animal/solcmp.sh
@@ -2,7 +2,7 @@ g++ -O2 -std=gnu++11 animal.cpp -o animal
 for i in {0..39}; do
 	echo "test $i"
 	./animal <test/$i.in >test/$i.ans
-	pypy animal.py <test/$i.in >test/$i.ans2
+	time pypy animal.py <test/$i.in >test/$i.ans2
 	diff test/$i.ans test/$i.ans2
 	rm test/$i.ans2
 done
